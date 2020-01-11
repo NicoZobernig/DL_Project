1a48e88a8c7d6938f7f24760a4f0fd4a9d5c0e64
Nicolas Zobernig <nico.zobernig@gmail.com>
2020-01-11 13:58:26 +0100

    training scripts

--- ZSL_Words_Triplet.py
+++ ZSL_Words_Triplet.py
@@ -1,49 +1,12 @@
 import torch
 import torch.cuda
 import numpy as np
-import argparse
 
 from torch.utils.data import DataLoader
 from zsldataset import ZSLDataset
 from models import ContinuousMap
 
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--leonhard', type=bool, default=False)
-    parser.add_argument('--use_irevnet', type=bool, default=True)  # use iRevNet features or ResNet101 features batch_size
-
-    parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--n_epochs', type=int, default=100)
-
-    parser.add_argument('--optimizer', type=str, default='sgd')
-
-    parser.add_argument('--learning_rate', type=float, default=1e-3)
-
-    parser.add_argument('--alphas', nargs='+', type=float, default=[20, 1e-3, 1e-3])
-
-    parser.add_argument('--margin', type=int, default=3)
-
-    parser.add_argument('--gamma', type=float, default=0.3)
-
-    parser.add_argument('--momentum', type=float, default=0.5)
-
-    return parser.parse_args()
-
-
-def dist_matrix(batch1, batch2):
-    delta = batch1.unsqueeze(1) - batch2.unsqueeze(0)
-    d_matrix = (delta * delta).mean(dim=-1)
-
-    return d_matrix
-
-
-def mag(u):
-    return torch.dot(u, u)
-
-
-def dist(u, v):
-    return torch.dot(u - v, u - v)
+from ZSL_Words_L2 import parse_args, dist_matrix
 
 
 def main():
@@ -65,7 +28,7 @@ def main():
     dim_semantic = trainset[0]['class_embedding'].shape[0]
     dim_visual = trainset[0]['image_embedding'].shape[0]
 
-    all_class_embeddings = torch.tensor(np.array(trainset.class_embeddings)).cuda().float()
+    all_class_embeddings = torch.tensor(np.array(trainset.class_embeddings)).float().cuda()
     classes_enum = torch.tensor(np.array(range(num_classes), dtype=np.int64)).cuda()
 
     query_ids = set([testset[i]['class_id'] for i in range(len(testset))])
@@ -81,13 +44,15 @@ def main():
         optimizer = torch.optim.Adam(list(v_to_s.parameters()) + list(s_to_v.parameters()),
                                      lr=options.learning_rate,
                                      betas=(0.9, 0.999),
-                                     weight_decay=3e-3)
+                                     weight_decay=1e-2)
     else:
         optimizer = torch.optim.SGD(list(v_to_s.parameters()) + list(s_to_v.parameters()),
                                     lr=options.learning_rate,
                                     momentum=options.momentum,
                                     weight_decay=5e-3,
                                     nesterov=True)
+
+    positive_part = torch.nn.ReLU().cuda()
 
     trainloader = DataLoader(trainset,
                              batch_size=options.batch_size,
@@ -103,9 +68,11 @@ def main():
                             pin_memory=True,
                             drop_last=True)
 
-    alpha1 = options.alphas[0]  # l2
+    alpha1 = options.alphas[0]  # triplet
     alpha2 = options.alphas[1]  # surjection
     alpha3 = options.alphas[2]  # l2 regularization
+
+    margin = options.margin
 
     # Main Loop
     for e in range(options.n_epochs):
@@ -116,7 +83,7 @@ def main():
         for i, sample in enumerate(trainloader):
             optimizer.zero_grad()
 
-            batch_visual = sample['image_embedding'].cuda().float()
+            batch_visual = sample['image_embedding'].float().cuda()
             batch_classes = sample['class_id'].cuda() - 1
 
             e_hat = v_to_s(s_to_v(all_class_embeddings))
@@ -128,16 +95,21 @@ def main():
             same_class = classes_enum.unsqueeze(0) == batch_classes.unsqueeze(1)
             same_class = same_class.detach()
 
-            l2_dist_loss = (dist_matrix(s_out, all_class_embeddings) * same_class.float()).mean()
+            d_matrix = dist_matrix(s_out, all_class_embeddings)
+
+            closest_negative, _ = (d_matrix + same_class.float() * 1e6).min(dim=-1)
+            furthest_positive, _ = (d_matrix * same_class.float()).max(dim=-1)
+
             l2_loss = (s_out * s_out).sum(dim=-1).mean()
-            loss = alpha1 * l2_dist_loss + alpha2 * surjection_loss + alpha3 * l2_loss
+            loss = positive_part(furthest_positive - closest_negative + margin)
+            loss = alpha1 * loss.mean() + alpha2 * surjection_loss + alpha3 * l2_loss
 
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
         else:
-            print('Training Loss epoch {0}: {1}'.format(e, running_loss/len(trainloader)))
+            print('Training Loss epoch {0}: {1}'.format(e+1, running_loss/len(trainloader)))
 
         if (e + 1) % 50 == 0:
             for param_group in optimizer.param_groups:
@@ -157,7 +129,7 @@ def main():
                 for i, sample in enumerate(testloader):
                     n += 1
 
-                    batch_visual = sample['image_embedding'].cuda().float()
+                    batch_visual = sample['image_embedding'].float().cuda()
                     batch_classes = sample['class_id'].cuda() - 1
 
                     s_out = v_to_s(batch_visual)
@@ -169,9 +141,10 @@ def main():
 
                     c_hat = (d_matrix + (1 - query_mask).float() * 1e6).argmin(dim=-1)
 
-                    l2_dist_loss = (dist_matrix(s_out, all_class_embeddings) * same_class.float()).mean()
+                    closest_negative, _ = (d_matrix + same_class.float() * 1e6).min(dim=-1)
+                    furthest_positive, _ = (d_matrix * same_class.float()).max(dim=-1)
 
-                    loss = alpha1 * l2_dist_loss
+                    loss = alpha1 * furthest_positive.mean()
 
                     avg_loss += loss.item()
                     avg_accuracy += (c_hat == batch_classes).float().mean().item()
